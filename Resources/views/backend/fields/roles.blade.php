6b010b3d8503df395e2bfe775fb9e840b1aa45af
Ralph Huwiler <ralph@huwiler.rocks>
2016-02-19 22:32:55 +0100

    update templates to emmit change event

--- Resources/views/backend/fields/roles.blade.php
+++ Resources/views/backend/fields/roles.blade.php
@@ -2,7 +2,7 @@
     @if(!isset($label) || $label != false)
         <label>{{ isset($label)?$label:trans('user::users.tabs.roles') }}</label>
     @endif
-    <select class="ui fluid search dropdown" multiple="" name="roles[]" id="rolesMultiselect" @if(isset($v_model)) v-model="{{$v_model}}"@endif>
+    <select class="ui fluid search dropdown" multiple="" name="roles[]" id="rolesMultiselect" @if(isset($v_model)) v-model="{{$v_model}}"@endif @if(isset($v_change))v-on:change="{{$v_change}}"@endif>
         @foreach ($roles as $role)
             <option value="{{ $role->id }}" @if(isset($user) && $user->hasRole($role->name)) selected @endif >{{ $role->display_name }}</option>
         @endforeach
--- Resources/views/backend/fields/users.blade.php
+++ Resources/views/backend/fields/users.blade.php
@@ -2,7 +2,7 @@
     @if(!isset($label) || $label != false)
         <label>{{ isset($label)?$label:trans('user::roles.title.users with this roles') }}</label>
     @endif
-    <select class="ui fluid search dropdown" multiple="" name="users[]" id="usersMultiselect" @if(isset($v_model)) v-model="{{$v_model}}"@endif>
+    <select class="ui fluid search dropdown" multiple="" name="users[]" id="usersMultiselect" @if(isset($v_model)) v-model="{{$v_model}}"@endif @if(isset($v_change))v-on:change="{{$v_change}}"@endif>
         @foreach ($users as $user)
             <option value="{{ $user->id }}" @if(isset($role) && $user->hasRole($role->name)) selected @endif >{{ $user->present()->fullname }}</option>
         @endforeach
