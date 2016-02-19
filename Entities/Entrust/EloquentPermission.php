b1c0f9b3ef4dbb9a63bdef1a59745a7a43234cf3
Ralph Huwiler <ralph@huwiler.rocks>
2016-02-19 09:59:19 +0100

    why did this work yesterday?

--- Entities/Entrust/EloquentPermission.php
+++ Entities/Entrust/EloquentPermission.php
@@ -4,6 +4,7 @@ namespace Modules\User\Entities\Entrust;
 use Zizaco\Entrust\Contracts\EntrustPermissionInterface;
 use Zizaco\Entrust\Traits\EntrustPermissionTrait;
 use Illuminate\Database\Eloquent\Model;
+use Config;
 
 class EloquentPermission extends Model implements EntrustPermissionInterface
 {
@@ -33,7 +34,7 @@ class EloquentPermission extends Model implements EntrustPermissionInterface
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function entrustRoles()
+    public function roles()
     {
         return $this->belongsToMany(Config::get('entrust.role'), Config::get('entrust.permission_role_table'),Config::get('entrust.permission_foreign_key'), Config::get('entrust.role_foreign_key'));
     }
