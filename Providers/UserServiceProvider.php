72b451e0ffdf55b20c7605ee836019014b3a879a
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-29 19:00:42 +0200

    core loads views

--- Providers/UserServiceProvider.php
+++ Providers/UserServiceProvider.php
@@ -35,8 +35,6 @@ class UserServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerMiddleware($this->app['router']);
-
-        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'user');
     }
 
     private function registerMiddleware($router)
@@ -78,40 +76,6 @@ class UserServiceProvider extends ServiceProvider
     }
 
     /**
-     * Register views.
-     *
-     * @return void
-     */
-    public function registerViews()
-    {
-        $viewPath = base_path('resources/views/modules/user');
-
-        $sourcePath = __DIR__.'/../Resources/views';
-
-        $this->publishes([
-            $sourcePath => $viewPath,
-        ]);
-
-        $this->loadViewsFrom([$viewPath, $sourcePath], 'user');
-    }
-
-    /**
-     * Register translations.
-     *
-     * @return void
-     */
-    public function registerTranslations()
-    {
-        $langPath = base_path('resources/lang/modules/user');
-
-        if (is_dir($langPath)) {
-            $this->loadTranslationsFrom($langPath, 'user');
-        } else {
-            $this->loadTranslationsFrom(__DIR__.'/../Resources/lang', 'user');
-        }
-    }
-
-    /**
      * Get the services provided by the provider.
      *
      * @return array
@@ -119,20 +83,5 @@ class UserServiceProvider extends ServiceProvider
     public function provides()
     {
         return [];
-    }
-
-    /**
-     * Register config.
-     *
-     * @return void
-     */
-    protected function registerConfig()
-    {
-        $this->publishes([
-            __DIR__.'/../Config/config.php' => config_path('user.php'),
-        ]);
-        $this->mergeConfigFrom(
-            __DIR__.'/../Config/config.php', 'user'
-        );
     }
 }
