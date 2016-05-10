903f247953ed82da537e0dfa9c47f83643eb039e
Ralph Huwiler <ralph@huwiler.rocks>
2016-05-10 20:56:05 +0200

    localizing module name

---
+++ Resources/lang/de/module.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'title'       => 'Benutzer',
+    'description' => 'Dieses Modul ist verantwortlich für die Verwaltung der Bentzer und deren Berechtigungen.',
+
+];
---
+++ Resources/lang/en/module.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'title'       => 'User',
+    'description' => 'The user module is responsible for managing users and permissions.',
+
+];
