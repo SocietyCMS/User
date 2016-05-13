cefebc7caa29ac4b5f59e3b037165b200afed622
Ralph Huwiler <ralph@huwiler.rocks>
2016-05-13 18:17:24 +0200

    remove activities table

--- Database/Seeders/GroupSeedTableSeeder.php
+++ Database/Seeders/GroupSeedTableSeeder.php
@@ -21,7 +21,6 @@ class GroupSeedTableSeeder extends Seeder
         DB::table('user__users')->delete();
         DB::table('user__roles')->delete();
         DB::table('user__password_resets')->delete();
-        DB::table('user__activities')->delete();
         DB::table('user__role_user')->delete();
         DB::table('user__permissions')->delete();
         DB::table('user__permission_role')->delete();
